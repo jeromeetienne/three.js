1bf57830f58d8b02ff696b782afa9525c21f66dc
Jerome Etienne <jerome.etienne@gmail.com>
2015-09-05 11:44:48 +0100

    added button to toggle sidebars

--- editor/js/Toolbar.js
+++ editor/js/Toolbar.js
@@ -12,6 +12,61 @@ var Toolbar = function ( editor ) {
 	var buttons = new UI.Panel();
 	container.add( buttons );
 
+	// toggle left sidebar
+
+	var toggleLeftSidebar = new UI.Button( '<<' ).setTitle('Toggle visibility of right sidebar')
+	toggleLeftSidebar.onClick( function () {
+		var domElement = document.querySelector('#leftSidebar')
+		var isVisible = domElement.style.display !== 'none' ? true : false
+		
+		if( isVisible ){
+
+			 document.querySelector('#leftSidebar').style.display = 'none'
+			 document.querySelector('#viewport').style.left = '0px'
+			 document.querySelector('#toolbar').style.left = '0px'
+			 toggleLeftSidebar.setLabel('>>')
+		}else{
+
+			 document.querySelector('#leftSidebar').style.display = ''
+			 document.querySelector('#viewport').style.left = '300px'
+			 document.querySelector('#toolbar').style.left = '300px'			
+			 toggleLeftSidebar.setLabel('<<')
+
+		}
+		
+		editor.signals.windowResize.dispatch();
+	} );
+	buttons.add( toggleLeftSidebar );
+
+
+	// toggle right sidebar
+
+	var toggleRightSidebar = new UI.Button( '>>' ).setTitle('Toggle visibility of right sidebar')
+	toggleRightSidebar.onClick( function () {
+		var domElement = document.querySelector('#sidebar')
+		var isVisible = domElement.style.display !== 'none' ? true : false
+		
+		if( isVisible ){
+
+			 document.querySelector('#sidebar').style.display = 'none'
+			 document.querySelector('#viewport').style.right = '0px'
+			 document.querySelector('#toolbar').style.right = '0px'
+			 toggleRightSidebar.setLabel('<<')
+		}else{
+
+			 document.querySelector('#sidebar').style.display = ''
+			 document.querySelector('#viewport').style.right = '300px'
+			 document.querySelector('#toolbar').style.right = '300px'			
+			 toggleRightSidebar.setLabel('>>')
+
+		}
+		
+		editor.signals.windowResize.dispatch();
+	} );
+	toggleRightSidebar.dom.style.cssFloat = 'right'
+	buttons.add( toggleRightSidebar );
+
+
 	// translate / rotate / scale
 
 	var translate = new UI.Button( 'translate' ).onClick( function () {
